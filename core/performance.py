b1997b1d6bb40b9e5c5fd5d684044bad281eb563
erickecastro <eerickevh@gmail.com>
2026-06-24 10:48:45 -0400

    perf(runtime): raise default FPS cap and enable SDL acceleration hints

--- core/performance.py
+++ core/performance.py
@@ -1,7 +1,7 @@
 import os
 
 
-TARGET_FPS = int(os.environ.get("PYOSU_TARGET_FPS", "1000"))
+TARGET_FPS = int(os.environ.get("PYOSU_TARGET_FPS", "480"))
 USE_BUSY_FRAME_PACER = os.environ.get("PYOSU_BUSY_FRAME_PACER", "0") == "1"
 DEBUG_PERFORMANCE = os.environ.get("PYOSU_DEBUG_PERFORMANCE", "0") == "1"
 MAX_FRAME_DT = 1 / 30
@@ -16,3 +16,6 @@ def configure_low_latency_environment():
     os.environ.setdefault("SDL_MOUSE_RELATIVE_MODE_WARP", "0")
     os.environ.setdefault("SDL_MOUSE_RELATIVE_SYSTEM_SCALE", "0")
     os.environ.setdefault("SDL_MOUSE_RELATIVE_MODE_CENTER", "0")
+    os.environ.setdefault("SDL_RENDER_SCALE_QUALITY", "0")
+    os.environ.setdefault("SDL_HINT_FRAMEBUFFER_ACCELERATION", "1")
+    os.environ.setdefault("SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS", "0")
